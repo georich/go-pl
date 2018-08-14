c48f315f7b2d1b78f4e51f1066326712fb6dea96
George Richards <georich@protonmail.com>
2018-08-14 12:21:42 +0100

    Examples of an echo implemtation (1 & 2)

---
+++ chapter1/echo2/echo2.go
@@ -0,0 +1,16 @@
+// prints its cmd line args
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
